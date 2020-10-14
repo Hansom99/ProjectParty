000c4f9b8667d87b04fcaaf833ff6c699559ca00
Hansom99 <phaensel@student.ethz.ch>
2020-10-14 20:49:10 +0200

    Done Charactermovement

--- ProjectParty/Assets/CharacterController2D.cs
+++ ProjectParty/Assets/CharacterController2D.cs
@@ -71,13 +71,13 @@ public class CharacterController2D : MonoBehaviour
 					m_CrouchDisableCollider.enabled = true;
 			}
 
-			// Move the character by finding the target velocity
-			Vector3 targetVelocity = new Vector2(move * 10f, m_Rigidbody2D.velocity.y);
-			// And then smoothing it out and applying it to the character
-			m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref velocity, m_MovementSmoothing);
+            // Move the character by finding the target velocity
+            Vector3 targetVelocity = new Vector2(move * 10f, m_Rigidbody2D.velocity.y);
+            // And then smoothing it out and applying it to the character
+            m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref velocity, m_MovementSmoothing);
 
-			// If the input is moving the player right and the player is facing left...
-			if (move > 0 && !m_FacingRight)
+            // If the input is moving the player right and the player is facing left...
+            if (move > 0 && !m_FacingRight)
 			{
 				// ... flip the player.
 				Flip();
--- ProjectParty/Assets/PlayerMovement.cs
+++ ProjectParty/Assets/PlayerMovement.cs
@@ -1,34 +1,46 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviourPunCallbacks {
 
 	public CharacterController2D controller;
 
-	Animator animator;
-	public GameObject shape;
+	//Animator animator;
+	//public GameObject shape;
+
+
 
 	public float runSpeed = 40f;
 
 	float horizontalMove = 0f;
 	bool jump = false;
 	bool crouch = false;
+	bool move = false;
+
+	Vector3 targetPos;
+	Vector3 zero = Vector3.zero;
+	float smooth = 0.9f;
+    private float speed = 1;
 
     private void Start()
     {
-		animator = shape.GetComponent<Animator>();
+		//animator = shape.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update () {
+
+		if (!photonView.IsMine) return;
 
 		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
 		if (Input.GetButtonDown("Jump"))
 		{
 			jump = true;
-			animator.SetBool("jump", jump);
+			//animator.SetBool("jump", jump);
+			
 		}
 
 		if (Input.GetButtonDown("Crouch"))
@@ -40,24 +52,47 @@ public class PlayerMovement : MonoBehaviour {
 		}
         if (Input.GetMouseButton(0))
         {
-			animator.SetBool("attack", true);
+			//animator.SetBool("attack", true);
 			Debug.Log("attack");
 		}
-		else animator.SetBool("attack", false);
+		//else animator.SetBool("attack", false);
 
 	}
 
 	void FixedUpdate ()
 	{
-		// Move our character
+		if (!photonView.IsMine) {
+			//transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref zero, smooth);
+			
+			return;
+		} 
+		// Move this character
+		
+
+		//Send to other Clients new Position
+		photonView.RPC("updatePosition", RpcTarget.Others, transform.position, horizontalMove * Time.fixedDeltaTime,crouch,jump);
 		controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+
 		jump = false;
-		animator.SetBool("jump", jump);
-		if (horizontalMove != 0) animator.SetBool("Running",true);
-		else animator.SetBool("Running", false);
+
+
+		//animator.SetBool("jump", jump);
+		//if (horizontalMove != 0) animator.SetBool("Running",true);
+		//else animator.SetBool("Running", false);
 		//Debug.Log(animator.GetFloat("speed"));
 		//animator.SetBool("attack", false);
 
+	}
+
+
+	[PunRPC]
+	void updatePosition(Vector3 pos,float move,bool crouch,bool jump)
+	{
+		transform.position = pos;
+		//this.move = true;
+		
+		
+		controller.Move(move, crouch, jump);
 	}
 
 }
