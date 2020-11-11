571036682e66287e55cd4fc8fe542f96815cb937
yayaa11 <yanick.buechel@gmail.com>
2020-11-11 21:09:38 +0100

    munitionsanzeige
    + zwei scripts für slider und text wie viel munition.

---
+++ ProjectParty/Assets/AmountOfAmmoText.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AmountOfAmmoText : MonoBehaviour
+{
+    public Text ammoAmount;
+    public void SetAmmo(int amount)
+    {
+        string numString = Convert.ToString(amount);
+        string ammoCount = "x" + numString;
+        ammoAmount.text = ammoCount;
+    }
+}
---
+++ ProjectParty/Assets/Scripts/AmmoBar/AmmoBar.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AmmoBar : MonoBehaviour
+{
+    public Slider slider;
+    public Image fill;
+    public Gradient gradient;
+    AmountOfAmmoText text;
+
+    public void SetMaxAmmo(int ammo)
+    {
+        slider.maxValue = ammo;
+        slider.value = ammo;
+
+        fill.color = gradient.Evaluate(1f);
+        text.SetAmmo(ammo);
+    }
+
+    public void SetAmmo(int ammo)
+    {
+        slider.value = ammo;
+        fill.color = gradient.Evaluate(slider.normalizedValue);
+        text.SetAmmo(ammo);
+    }
+}
